b29ac46d41ae6c9ca8555bc4a98fb909e7090ebe
Ubuntu <ubuntu@ip-10-232-15-88.ec2.internal>
2024-03-26 02:13:52 +0000

    clean and fix minor bugs

--- scripts/visualizations/visualize_zed_depth.py
+++ scripts/visualizations/visualize_zed_depth.py
@@ -4,9 +4,72 @@ import numpy as np
 import os
 import fnmatch
 
-from r2d2.trajectory_utils.misc import load_trajectory
-from r2d2.camera_utils.recording_readers.svo_reader import SVOReader
+from droid.trajectory_utils.misc import load_trajectory
+from droid.camera_utils.recording_readers.svo_reader import SVOReader
 
+
+# Get paths
+filepath = "INSERT PATH TO DATA FOLDER HERE (eg. /path/to/folder/Fri_Jul__7_14:57:48_2023)"
+traj_filepath = os.path.join(filepath, "trajectory.h5")
+recording_folderpath = os.path.join(filepath, "recordings/SVO")
+svo_files = []
+for root, _, files in os.walk(recording_folderpath):
+    for filename in files:
+        if fnmatch.fnmatch(filename, "*.svo"):
+            svo_files.append(os.path.join(root, filename))
+
+
+# Get intrinsics information and camera IDs
+frame_counts = []
+serial_numbers = []
+cam_matrices = []
+cam_baselines = []
+for svo_file in svo_files:
+    # Open SVO Reader
+    serial_number = svo_file.split("/")[-1][:-4]
+    camera = SVOReader(svo_file, serial_number=serial_number)
+    camera.set_reading_parameters(image=True, pointcloud=False, concatenate_images=False)
+    im_key = '%s_left' % serial_number
+    # Intrinsics are the same for the left and the right camera
+    cam_matrices.append(camera.get_camera_intrinsics()[im_key]['cameraMatrix'])
+    cam_baselines.append(camera.get_camera_baseline())
+    frame_count = camera.get_frame_count()
+    frame_counts.append(frame_count)
+    serial_numbers.append(serial_number)
+
+
+# Get camera intrinsics and baselines just in case someone wants to use a learned
+# stereo model which consumes these
+cam_matrices = [x for y, x in sorted(zip(serial_numbers, cam_matrices))]
+cam_baselines = [x for y, x in sorted(zip(serial_numbers, cam_baselines))]
+serial_numbers = sorted(serial_numbers)
+
+assert frame_counts.count(frame_counts[0]) == len(frame_counts)
+
+
+# Load trajectory and associated images
+traj = load_trajectory(traj_filepath, recording_folderpath=recording_folderpath, camera_kwargs={cam_type: {"depth": True} for cam_type in ["varied_camera", "hand_camera"]})
+
+timestep = np.random.randint(frame_counts[0])
+frame = traj[timestep]
+obs = frame["observation"]
+image_obs = obs["image"]
+depth_obs = obs["depth"]
+
+zed_depths = []
+left_rgbs = []
+right_rgbs = []
+for i, cam_id in enumerate(serial_numbers):
+    left_key, right_key = f"{cam_id}_left", f"{cam_id}_right"
+    left_rgb, right_rgb = image_obs[left_key], image_obs[right_key] 
+    left_rgbs.append(left_rgb)
+    right_rgbs.append(right_rgb)
+    # Note (Ashwin): depth from the left and right stereo pairs are the same
+    # so I'm just arbitrarily picking one
+    zed_depths.append(depth_obs[left_key])
+
+
+# Do plotting
 def make_cv_disparity_image(disparity, max_disparity):
     vis_disparity = disparity / max_disparity
     vis_disparity[vis_disparity < 0.0] = 0.0
@@ -18,64 +81,6 @@ def make_cv_disparity_image(disparity, max_disparity):
     mapped[vis_disparity > 1.0 - 1e-3, :] = 0
     return mapped
 
-filepath = "INSERT PATH TO DATA FOLDER HERE (eg. /path/to/folder/Fri_Jul__7_14:57:48_2023)"
-traj_filepath = os.path.join(filepath, "trajectory.h5")
-recording_folderpath = os.path.join(filepath, "recordings/SVO")
-traj = load_trajectory(traj_filepath, recording_folderpath=recording_folderpath, camera_kwargs={})
-
-svo_files = []
-for root, _, files in os.walk(recording_folderpath):
-    for filename in files:
-        if fnmatch.fnmatch(filename, "*.svo"):
-            svo_files.append(os.path.join(root, filename))
-
-cameras = []
-frame_counts = []
-serial_numbers = []
-cam_matrices = []
-cam_baselines = []
-
-for svo_file in svo_files:
-    # Open SVO Reader
-    serial_number = svo_file.split("/")[-1][:-4]
-    camera = SVOReader(svo_file, serial_number=serial_number)
-    camera.set_reading_parameters(image=True, depth=True, pointcloud=False, concatenate_images=False)
-    im_key = '%s_left' % serial_number
-    # Intrinsics are the same for the left and the right camera
-    cam_matrices.append(camera.get_camera_intrinsics()[im_key]['cameraMatrix'])
-    cam_baselines.append(camera.get_camera_baseline())
-    frame_count = camera.get_frame_count()
-    cameras.append(camera)
-    frame_counts.append(frame_count)
-    serial_numbers.append(serial_number)
-
-# return serial_numbers
-cameras = [x for y, x in sorted(zip(serial_numbers, cameras))]
-cam_matrices = [x for y, x in sorted(zip(serial_numbers, cam_matrices))]
-cam_baselines = [x for y, x in sorted(zip(serial_numbers, cam_baselines))]
-serial_numbers = sorted(serial_numbers)
-
-assert frame_counts.count(frame_counts[0]) == len(frame_counts)
-
-timestep = np.random.randint(frame_counts[0])
-frame = traj[timestep]
-obs = frame["observation"]
-image_obs = obs["image"]
-depth_obs = obs["depth"]
-
-zed_depths = []
-left_rgbs = []
-right_rgbs = []
-
-for i, cam_id in enumerate(serial_numbers):
-    left_key, right_key = f"{cam_id}_left", f"{cam_id}_right"
-    left_rgb, right_rgb = image_obs[left_key], image_obs[right_key] 
-    left_rgbs.append(left_rgb)
-    right_rgbs.append(right_rgb)
-    # Note (Ashwin): depth from the left and right stereo pairs are the same
-    # so I'm just arbitrarily picking one
-    zed_depths.append(depth_obs[left_key])
-
 images = []
 for serial_num, zed_depth, left_rgb, right_rgb in zip(serial_numbers, zed_depths, left_rgbs, right_rgbs):
     zed_depth[np.isnan(zed_depth)] = 0
@@ -85,11 +90,9 @@ for serial_num, zed_depth, left_rgb, right_rgb in zip(serial_numbers, zed_depths
     images.append(left_rgb)
     images.append(right_rgb)
     images.append(zed_depth_vis)
-
 # Create a 3x3 subplot grid
 fig, axes = plt.subplots(3, 3, figsize=(12, 5))
 titles = ["Left RGB", "Right RGB", "ZED Depth"] * 3
-
 # Iterate through the images and display them on the subplots
 for i, ax in enumerate(axes.ravel()):
     if i < len(images):
