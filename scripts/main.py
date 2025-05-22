62094d49ce9d42c97b7e5c602fd42a70528ba1b0
Arhan Jain <arhan.j04@gmail.com>
2025-05-21 22:22:23 -0400

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -1,3 +1,19 @@
+# Fix for Tkinter in UV
+import tkinter
+from os import environ
+from pathlib import Path
+from sys import base_prefix
+import platform
+
+if not ("TCL_LIBRARY" in environ and "TK_LIBRARY" in environ):
+    try:
+        tkinter.Tk()
+    except tkinter.TclError:
+        tk_dir = "tcl" if platform.system() == "Windows" else "lib"
+        tk_path = Path(base_prefix) / tk_dir
+        environ["TCL_LIBRARY"] = str(next(tk_path.glob("tcl8.*")))
+        environ["TK_LIBRARY"] = str(next(tk_path.glob("tk8.*")))
+
 from droid.controllers.oculus_controller import VRPolicy
 from droid.robot_env import RobotEnv
 from droid.user_interface.data_collector import DataCollecter
